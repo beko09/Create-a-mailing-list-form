aae927b2636e6d1da2ec0f5f599b92cfa16c8e6e
abobaker hilal <38497282+beko09@users.noreply.github.com>
2019-04-06 18:07:23 +0200

    Update views.py

--- post/views.py
+++ post/views.py
@@ -9,41 +9,50 @@ from django.core.mail import send_mail, get_connection,BadHeaderError,EmailMulti
 
 
 
+#In this function the user will log in and send him a thank you messagein this function user rgister and send email
 
 def newsletter_singup(request):
-    now = timezone.now()
     form = NewsletterForm(request.POST or None)
     if form.is_valid():
         instance = form.save(commit=False)
+        #Here I am sure that the email is registered or not
         if NewsLetterUser.objects.filter(email=instance.email).exists():
+            #if email register show  messages warning
             messages.warning(request,f'هذا الايميل مشترك مسبقا')
         else:
+            # if email not register save this email and show messages success
             instance.save()
             messages.success(request,f'تم الاشتراك بنجاح ')
+            #l am use function send() to send email
             subject = " الاشتراك البريدي"
             from_email = settings.EMAIL_HOST_USER
             to_email = [instance.email]
+            #Here you can use an external text file so that you type what you want
             # with open(settings.BASE_DIR + "/templates/sign_up_email.txt") as f:
             #     signup_message = f.read()
+            # lam use simle text
             signup_message = """ helo wel come """
             message = EmailMultiAlternatives(subject=subject,body=signup_message,from_email=from_email,to=to_email)
             html_template = get_template("sign_up_email.html").render()
             message.attach_alternative(html_template,"text/html")
             message.send()
     context ={
-         'year':now,
         'form': form
     }   
     return render(request,'newsletter.html', context)
 
+#In this function I cancel the subscription 
 def newsletter_unsubscribe(request):
-    now = timezone.now()
     form = NewsletterForm(request.POST or None)
     if form.is_valid():
         instance = form.save(commit=False)
+        #I first verify that the email entered by the user is in a table NewsLetterUser
         if NewsLetterUser.objects.filter(email=instance.email).exists():
+            #if email in table then delete this email and  Sending a farewell message
             NewsLetterUser.objects.filter(email=instance.email).delete()
             messages.success(request,f'تم الغاء الاشتراك  ')
+            #"""Here I bring the email form and not from the table because the email was
+           # already cleared from the table and then I send a farewell message"""
             subject = "شكرا للك"
             from_email = settings.EMAIL_HOST_USER
             to_email = [instance.email]
@@ -57,16 +66,17 @@ def newsletter_unsubscribe(request):
         else:
             messages.warning(request,f'هذا الايميل غير موجود ')
     context ={
-         'year':now,
         'form': form
     }   
     return render(request,'newsletter_unsubscribe.html', context)
 
 
 
-
+#In this function I send new news to those who have registered in the mailing list
 def control_newsletter(request):
+    #I bring the amyla from a table NewsLetterUser I only take the values of the field
     getemail = NewsLetterUser.objects.all().values_list('email',flat=True).distinct()
+    #And then convert the values  to list to ease send with function send_email
     email2=list(getemail)
     if request.method == 'POST':
         form = NewsCreationForm(request.POST)
