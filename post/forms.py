8565f6e08096e6e8e220f681da8cf3f24d265cfd
abobaker hilal <38497282+beko09@users.noreply.github.com>
2019-04-06 18:12:54 +0200

    Update forms.py

--- post/forms.py
+++ post/forms.py
@@ -1,20 +1,22 @@
 from django import forms
 from .models import NewsLetterUser
 
+#Here, create a form, signup form and submit news
 
+#form signup to use model form ==>NewsLetterUser
 class NewsletterForm(forms.ModelForm):
     email = forms.EmailField(required=True, label=' الايميل')
     class Meta:
         model = NewsLetterUser
         fields = ['email']
-
+        #validte email
         def clean_email(self):
             email = self.cleaned_data.get('email')
             return email
 
 
 
-
+#form news but use Form only no model form
 class NewsCreationForm(forms.Form):
     subject = forms.CharField(max_length=100)
     message = forms.CharField(widget=forms.Textarea)
